86916b2e570bfdbe5b59f72f5d324fcddced81f6
Mayowa Ibitola <mrdavidtola@gmail.com>
2022-11-02 19:19:55 +0100

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -75,11 +75,8 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + 'db.sqlite3',
     }
 }
 
